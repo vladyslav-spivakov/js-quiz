ab4a7d24c32a36e8cb589d1d30e0e24f31c2c155
Vladyslav Spivakov <funnydeiv@gmail.com>
2019-12-22 09:51:22 +0000

    sdssd

--- js/index.js
+++ js/index.js
@@ -1,7 +1,10 @@
-firstQuestion = 5;
-questionNum   = firstQuestion-1;
-question      = [];
-questions     = [];
+const firstQuestion = 1;
+let questionNum   = firstQuestion-1;
+let question      = [];
+let questions     = [];
+let questionMax   = 4;
+let correctAnswers= 0;
+let quizStatus = "";
 
 document.addEventListener('DOMContentLoaded',init());
 function init(){
@@ -14,11 +17,54 @@ function init(){
     questions = JSON.parse(xhr.responseText);
     console.log(questions);
     }
+    questionMax = questions.length-1;
+    buildQuestion();
+    function buildQuestion() {
+        question=questions[questionNum];
+        console.log(question);
+        document.querySelector("#quiz-question").innerHTML = question.question;
+        document.querySelector("label[for=ans1]").innerHTML = question.answer1;
+        document.querySelector("label[for=ans2]").innerHTML = question.answer2;
+        document.querySelector("label[for=ans3]").innerHTML = question.answer3;
+    }
+    document.querySelector(".quiz-answers input[id=ans1]").addEventListener('click', ()=> {
+        questions[questionNum].status = 1;
+        if (questionNum !== questionMax) {
+            questionNum++;
+            buildQuestion();
+        } else if (quizStatus !== "end") {
+            buildResult();
+        }
+    });
     
-    question=questions[questionNum];
-    console.log(question);
-    document.querySelector("#quiz-question").innerHTML = question.question;
-    document.querySelector("label[for=ans1]").innerHTML = question.answer1;
-    document.querySelector("label[for=ans2]").innerHTML = question.answer2;
-    document.querySelector("label[for=ans3]").innerHTML = question.answer3;
+    document.querySelector(".quiz-answers input[id=ans2]").addEventListener('click', ()=> {
+        questions[questionNum].status = 2;
+        if (questionNum !== questionMax) {
+            questionNum++;
+            buildQuestion();
+        } else if (quizStatus !== "end") {
+            buildResult();
+        }
+    });
+    document.querySelector(".quiz-answers input[id=ans3]").addEventListener('click', ()=> {
+        questions[questionNum].status = 3;
+        if (questionNum !== questionMax) {
+            questionNum++;
+            buildQuestion();
+        } else if (quizStatus !== "end") {
+            buildResult();
+        }
+    });
+    function buildResult() {
+        quizStatus="end";
+        for (let i = 0; i<=questionMax; i++) {
+            if (questions[i].status === questions[i].answer) {
+                correctAnswers++;
+            }
+        }
+        document.querySelector("label[for=ans1]").innerHTML = "";
+        document.querySelector("label[for=ans2]").innerHTML = "";
+        document.querySelector("label[for=ans3]").innerHTML = "";
+        document.querySelector("#quiz-question").innerHTML = "Ви правильно відповили на <span class='greenCorrect'>" + correctAnswers + "</span> з <span class='answersNum'>" + (questionMax+1) + "</span> питань!";
+    }
 };
